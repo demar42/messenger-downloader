4cdd8c402c3fd959a7a90e45596a2c7fcf883114
Daniel Mar <mardaniel42@gmail.com>
2017-03-01 21:00:26 +1300

    Inital commit - pre rebuild of getUserInfo

---
+++ read.js
@@ -0,0 +1,94 @@
+const login = require("facebook-chat-api");
+const fs = require("fs");
+const prompt = require("syncprompt")
+
+var threadIDs = [];
+var threadNames = [];
+var threadTimeStamps = [];
+var threadPosition;
+var reverseAmount;
+
+login({
+    appState: JSON.parse(fs.readFileSync('loginFile.json', 'utf8'))
+}, (err, api) => {
+    if (err) return console.log(err);
+
+    api.getThreadList(0, 20, (err, threads) => {
+        if (err) return console.err(err);
+
+        console.log("-----------List of conversations------------");
+
+        for (var i = 0; i < threads.length; i++) {
+            threadNames.push(threads[i].name.toLowerCase());
+            threadIDs.push(threads[i].threadID);
+            threadTimeStamps.push(threads[i].timestamp);
+            console.log(threadNames[i]);
+        }
+
+        console.log("Which conversation do you want to download?");
+        getConversationName();
+
+    });
+
+    function getConversationName() {
+        var conversationName = prompt("name >>");
+        threadPosition = threadNames.indexOf(conversationName);
+        if (threadPosition == -1) {
+            console.log("Invalid Name!");
+            getConversationName();
+        } else {
+            console.log("Enter the number of messages you would like to download");
+            getNumberOfMessages();
+        }
+    }
+
+    function getNumberOfMessages() {
+        reverseAmount = parseInt(prompt("amount >>"));
+        if (isNaN(reverseAmount)) {
+            console.log("Invalid Number!");
+            getNumberOfMessages();
+        } else {
+            getPastMessages();
+        }
+    }
+
+    function getPastMessages() {
+        var threadID = threadIDs[threadPosition];
+
+        api.getThreadInfo(threadID, (err, info) => {
+            if (err) return console.err(err);
+
+            var lastmsg = info.messageCount
+            var firstmsg = lastmsg - reverseAmount;
+
+            api.getThreadHistory(threadID, firstmsg, lastmsg, threadTimeStamps[threadPosition], (err, messages) => {
+                var dump = '';
+                var endLength = messages.length - 1;
+                console.log(endLength);
+                var i = 0
+                for (var loop = 0; loop < messages.length; loop++) {
+                    var name;
+                    var sender = messages[loop].senderID.slice(5);
+                    console.log(sender);
+                    api.getUserInfo(sender, (err, person) => {
+                        if (err) return console.log("error");
+                        var body = messages[i].body;
+                        console.log(messages[i].senderID);
+                        console.log(Object.keys(person));
+                        name = person[messages[i].senderID.slice(5)].firstName;
+                        dump += name + ": \n";
+                        dump += body + "\n----------------------------\n\n";
+                        if (i == endLength) {
+                            console.log(dump);
+                            fs.writeFile('messages.txt', dump, err => {
+                                console.log('All done!');
+                                process.exit(0);
+                            });
+                        }
+                        i++;
+                    });
+                }
+            });
+        });
+    }
+});
